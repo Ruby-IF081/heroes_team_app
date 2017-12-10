6591759a5b8e117db34fdfc2e89b1a14ba6d6db8
cervols <tania.zavediuk@gmail.com>
2017-12-10 16:37:43 -0500

    Add validation rules and rspec tests for the Tenant model

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,3 +1,8 @@
 class Tenant < ApplicationRecord
   has_many :users
+  validates :name, presence: true, length: { minimum: 3, maximum: 64 }
+  VALID_WEBSITE_REGEX = /\A[-a-z0-9_]+\.+[-a-z0-9\/]+\z/i
+  validates :website, presence: true, length: { minimum: 3, maximum: 64 },
+	    format: { with: VALID_WEBSITE_REGEX },
+	    uniqueness: { case_sensitive: false }
 end
--- spec/models/tenant_spec.rb
+++ spec/models/tenant_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Tenant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'is valid with name, phone, website and logo' do
+     tenant = Tenant.new(
+       name:    'Heroes co.',
+       phone:   '1-800-291-9249',
+       website: 'heroes.com',
+       logo:    ''
+    )
+    expect(tenant).to be_valid
+  end
+
+  # it { should have_many(:users).dependent(:destroy) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:website) }
+  it { should validate_uniqueness_of(:website).case_insensitive }
+
+  it do
+    should validate_length_of(:name).
+      is_at_least(3).is_at_most(64)
+  end
+
+  it do
+    should validate_length_of(:website).
+      is_at_least(3).is_at_most(64)
+  end
 end
