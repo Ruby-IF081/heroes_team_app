3a840064fd0fca715af5dadade30aa9453060a72
cervols <tania.zavediuk@gmail.com>
2017-12-15 02:27:50 +0200

    [-] Remove specs for ActiveRecord validations

--- spec/models/tenant_spec.rb
+++ spec/models/tenant_spec.rb
@@ -4,14 +4,4 @@ RSpec.describe Tenant, type: :model do
   it 'has a valid factory' do
     expect(build(:tenant)).to be_valid
   end
-
-  it do
-    should validate_length_of(:name)
-      .is_at_least(3).is_at_most(64)
-  end
-
-  it do
-    should validate_length_of(:website)
-      .is_at_least(3).is_at_most(64)
-  end
 end
