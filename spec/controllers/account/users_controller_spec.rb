f5e914c0a8c4aba63c768ba6bdb3cf1e30571993
Misha Push <vmisha7776@gmail.com>
2018-01-12 22:40:17 +0200

    Covered users controller with test on 100% (#115)
    * Covered users controller with test on 100%
    
    * Added test to check what current_user is after and before impersonate action
    
    * Improved alert html
    
    * Improved the bug with impersonate test

--- spec/controllers/account/users_controller_spec.rb
+++ spec/controllers/account/users_controller_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe Account::UsersController, type: :controller do
   end
 
   describe 'action #index' do
-    it 'get the user of current tenant' do
+    it 'should get the user of current tenant' do
       get :index
       expect(assigns(:users)).to contain_exactly(@user)
     end
-    it 'renders the :index view' do
+    it 'should render the :index view' do
       get :index
       expect(response).to have_http_status(200)
       expect(response).to render_template(:index)
@@ -19,11 +19,11 @@ RSpec.describe Account::UsersController, type: :controller do
   end
 
   describe 'action #new' do
-    it 'initiate empty user' do
+    it 'should initiate empty user' do
       get :new
       expect(assigns(:user)).to be_a_new(User)
     end
-    it 'renders the :new view' do
+    it 'should render the :new view' do
       get :new
       expect(response).to have_http_status(200)
       expect(response).to render_template(:new)
@@ -31,11 +31,11 @@ RSpec.describe Account::UsersController, type: :controller do
   end
 
   describe 'action #show' do
-    it 'assigns the requested user to @user' do
+    it 'should assign the requested user to @user' do
       get :show, params: { id: @user.id }
       expect(assigns(:user)).to eq(@user)
     end
-    it 'renders the :show view' do
+    it 'should render the :show view' do
       get :show, params: { id: @user.id }
       expect(response).to have_http_status(200)
       expect(response).to render_template(:show)
@@ -46,7 +46,7 @@ RSpec.describe Account::UsersController, type: :controller do
     context 'with valid attributes' do
       let!(:val_user) { build(:user) }
 
-      it 'saves a new user' do
+      it 'should save a new user' do
         expect do
           post :create, params: { user: { first_name: val_user.first_name,
                                           last_name: val_user.last_name,
@@ -54,7 +54,7 @@ RSpec.describe Account::UsersController, type: :controller do
         end.to change(User, :count).by(1)
       end
 
-      it 'redirects to the all users' do
+      it 'should redirect to the all users' do
         post :create, params: { user: { first_name: val_user.first_name,
                                         last_name: val_user.last_name,
                                         email: val_user.email } }
@@ -64,15 +64,15 @@ RSpec.describe Account::UsersController, type: :controller do
       end
     end
     context 'with invalid attributes' do
-      it 'does not creates a new user' do
+      it 'should not create a new user' do
         expect do
           post :create, params: { user: { first_name: 'Jo',
                                           last_name: nil,
                                           email: nil } }
-        end.to_not change(Company, :count)
+        end.to_not change(User, :count)
       end
 
-      it 'renders the new action again' do
+      it 'should render the new action again' do
         post :create, params: { user: { first_name: 'Jo',
                                         last_name: nil,
                                         email: nil } }
@@ -85,7 +85,7 @@ RSpec.describe Account::UsersController, type: :controller do
 
   describe 'action #update' do
     context 'valid attributes' do
-      it 'located the requested user' do
+      it 'should locate the requested user' do
         put :update, params: { id: @user.id,
                                user: { first_name: @user.first_name,
                                        last_name: @user.last_name,
@@ -93,7 +93,7 @@ RSpec.describe Account::UsersController, type: :controller do
         expect(assigns(:user)).to eq(@user)
       end
 
-      it "changes user's attributes" do
+      it "should change user's attributes" do
         put :update, params: { id: @user.id,
                                user: { first_name: 'Firstname',
                                        last_name: 'Lastname',
@@ -104,7 +104,7 @@ RSpec.describe Account::UsersController, type: :controller do
         expect(@user.email).to eq('email@mail.com')
       end
 
-      it 'redirects to the users table' do
+      it 'should redirect to the users table' do
         put :update, params: { id: @user.id,
                                user: { first_name: 'Firstname',
                                        last_name: 'Lastname',
@@ -116,7 +116,7 @@ RSpec.describe Account::UsersController, type: :controller do
     end
 
     context 'invalid attributes' do
-      it 'located the requested user' do
+      it 'should locate the requested user' do
         put :update, params: { id: @user.id,
                                user: { first_name: @user.first_name,
                                        last_name: @user.last_name,
@@ -124,7 +124,7 @@ RSpec.describe Account::UsersController, type: :controller do
         expect(assigns(:user)).to eq(@user)
       end
 
-      it "does not change user's attributes" do
+      it "should not change user's attributes" do
         put :update, params: { id: @user.id,
                                user: { first_name: nil,
                                        last_name: nil,
@@ -135,7 +135,7 @@ RSpec.describe Account::UsersController, type: :controller do
         expect(@user.email).to_not eq(nil)
       end
 
-      it 're-renders the edit method' do
+      it 'should re-renders the edit method' do
         put :update, params: { id: @user.id,
                                user: { first_name: nil,
                                        last_name: nil,
@@ -147,16 +147,46 @@ RSpec.describe Account::UsersController, type: :controller do
     end
   end
 
-  describe 'action destroy' do
-    it 'deletes the user' do
+  describe 'action #edit' do
+    it 'should get the user data for editing' do
+      get :edit, params: { id: @user.id }
+      expect(response).to have_http_status(200)
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'action destroy a user' do
+    it 'should delete the user' do
       expect { delete :destroy, params: { id: @user.id } }.to change(User, :count).by(-1)
     end
 
-    it 'redirects to user#index' do
+    it 'should redirect to user#index' do
       delete :destroy, params: { id: @user.id }
       expect(response).to have_http_status(302)
       expect(response).to redirect_to account_users_path
       expect(controller).to set_flash[:success]
+    end
+  end
+
+  describe 'action impersonate and stop_impersonating' do
+    let!(:pretended_user) do
+      create(:user, tenant_id: @user.tenant_id)
+    end
+
+    it 'should check if current tenant has the user to act as' do
+      get :index
+      expect(assigns(:users)).to contain_exactly(@user, pretended_user)
+    end
+
+    it 'admin should pretend as another user' do
+      expect(subject.current_user).to eq(@user)
+      post :impersonate, params: { id: pretended_user.id }
+      expect(subject.current_user).not_to eq(@user)
+      expect(response).to redirect_to root_path
+      expect(subject.current_user).to eq(pretended_user)
+      post :stop_impersonating
+      expect(subject.current_user).to eq(@user)
+      expect(response).to redirect_to root_path
     end
   end
 end
