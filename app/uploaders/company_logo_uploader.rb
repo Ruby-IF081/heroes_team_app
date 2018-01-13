0a11267b4c3d9db205f500a1fbf3051af5631051
dovganych <misha.dovganych@gmail.com>
2018-01-13 21:11:22 +0200

    Fix images on production, add resizing images, change default company logo

--- app/uploaders/company_logo_uploader.rb
+++ app/uploaders/company_logo_uploader.rb
@@ -1,9 +1,13 @@
 class CompanyLogoUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  process resize_and_pad: [150, 150, :transparent, "Center"]
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def default_url
-    "/assets/" + [version_name, "default.png"].compact.join('_')
+    "fallback/default.png"
   end
 end
