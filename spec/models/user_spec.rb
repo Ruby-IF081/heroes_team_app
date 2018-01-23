5aa482b5bc31d482ece8e904f0b93a238f2dda6e
Serhii Bodnaruk <32734554+sergiwez@users.noreply.github.com>
2018-01-23 11:52:03 +0200

    Added tests (#167)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 describe User, type: :model do
+  let!(:user) { build(:user, :sale, first_name: 'Winston', last_name: 'Churchill') }
+
   it { should belong_to(:tenant) }
   it { should have_many(:companies) }
   it { should accept_nested_attributes_for(:tenant) }
+
+  it 'user full name should be correct' do
+    expect(user.full_name).to eq('Winston Churchill')
+  end
 end
