c9169aa04a006940a6506d6a49b9061c4cdc22f2
dovganych <misha.dovganych@gmail.com>
2018-01-23 20:31:39 +0200

    update tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,25 @@
 require 'rails_helper'
 
 describe User, type: :model do
-  let!(:user) { build(:user) }
+  let!(:user) { build(:user, :sale, first_name: 'Winston', last_name: 'Churchill') }
 
   it { should belong_to(:tenant) }
   it { should have_many(:companies) }
   it { should accept_nested_attributes_for(:tenant) }
-  it "full_name method" do
-    expect(user.full_name).to eq("#{user.first_name} #{user.last_name}")
+
+  it 'user full name should be correct' do
+    expect(user.full_name).to eq('Winston Churchill')
   end
-  it "admin? method" do
-    user.role = User::ADMIN_ROLE
-    expect(user.admin?).to be true
+
+  it "admin? method should be correct" do
+    expect(user.admin?).to be false
   end
-  it "super_admin? method" do
-    user.role = User::SUPER_ADMIN_ROLE
-    expect(user.super_admin?).to be true
+
+  it "super_admin? method should be correct" do
+    expect(user.super_admin?).to be false
+  end
+
+  it "privileged? method should be correct" do
+    expect(user.privileged?).to be false
   end
 end
