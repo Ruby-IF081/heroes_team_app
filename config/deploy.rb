3ec3405489a589b7d0c1ca1da0459e1803f279af
Serhii Bodnaruk <32734554+sergiwez@users.noreply.github.com>
2018-02-03 16:40:36 +0200

    Added uploads folder to linked dirs (#198)

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,8 @@ set :format_options, command_output: true, log_file: "log/capistrano.log", color
 append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets",
+       "public/system", "public/uploads"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
