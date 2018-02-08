24fd1c20cfa05d3068b651a046a12a13a546915a
cervols <tania.zavediuk@gmail.com>
2018-02-08 22:00:59 +0200

    Improve rspec

--- spec/controllers/sitemap_controller_spec.rb
+++ spec/controllers/sitemap_controller_spec.rb
@@ -7,10 +7,12 @@ RSpec.describe SitemapController, type: :controller do
       expect(response).to have_http_status(:success)
     end
 
-    it 'has all fields' do
+    it 'has all links with all fields' do
       get :index, format: :xml
       response_xml = Nokogiri::XML(response.body)
-      response_xml.xpath('//xmlns:url').each do |url|
+      response_url_path = response_xml.xpath('//xmlns:url')
+      expect(response_url_path.count).to be_equal(4)
+      response_url_path.each do |url|
         expect(url.xpath('xmlns:loc').text).to be_present
         expect(url.xpath('xmlns:changefreq').text).to be_present
         expect(url.xpath('xmlns:priority').text).to be_present
