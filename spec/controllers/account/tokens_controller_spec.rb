d60a4c553734fe134a8adc1dce38cf38e81d78d7
Anton Chupak <antonchupack@gmail.com>
2018-02-09 14:54:55 +0200

    API_token generated&deactivated through AJAX (#209)
    * API_token generated&deactivated through AJAX
    
    * Improved javascript with replaceWith

--- spec/controllers/account/tokens_controller_spec.rb
+++ spec/controllers/account/tokens_controller_spec.rb
@@ -8,37 +8,37 @@ RSpec.describe Account::TokensController, type: :controller do
     it 'should change users token attributes' do
       old_token = user.auth_token
       old_time = user.token_created_at
-      post :create
+      post :create, xhr: true
       user.reload
 
       expect(user.auth_token).not_to eq(old_token)
       expect(user.token_created_at).not_to eq(old_time)
     end
 
-    it 'redirects to the user show' do
-      post :create
+    it 'should not redirect to the profile show' do
+      post :create, xhr: true
 
-      expect(response).to have_http_status(302)
-      expect(response).to redirect_to(account_profile_path)
-      expect(controller).to set_flash[:success]
+      expect(response).not_to have_http_status(302)
+      expect(response).not_to redirect_to(account_profile_path)
+      expect(controller).not_to set_flash[:success]
     end
   end
 
   describe 'action #deactivate_token' do
     it 'should destroy users token attributes' do
-      delete :destroy
+      delete :destroy, xhr: true
       user.reload
 
       expect(user.auth_token).to eq(nil)
       expect(user.token_created_at).to eq(nil)
     end
 
-    it 'redirects to the user show' do
-      delete :destroy
+    it 'should not redirect to the profile show' do
+      delete :destroy, xhr: true
 
-      expect(response).to have_http_status(302)
-      expect(response).to redirect_to(account_profile_path)
-      expect(controller).to set_flash[:info]
+      expect(response).not_to have_http_status(302)
+      expect(response).not_to redirect_to(account_profile_path)
+      expect(controller).not_to set_flash[:info]
     end
   end
 end
