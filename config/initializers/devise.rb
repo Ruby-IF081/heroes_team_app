1cadca5e79d5152fe30e6c5cdc2e484381f0c036
Misha7776 <vmisha7776@gmail.com>
2018-02-14 19:05:49 +0200

    Used the facebook js SDK for sign up through popup

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -253,7 +253,8 @@ Devise.setup do |config|
   config.omniauth :facebook,
                   Rails.application.secrets.facebook_app_id,
                   Rails.application.secrets.facebook_app_secret,
-                  scope: 'email', info_fields: 'email, first_name, last_name'
+                  scope: 'email', info_fields: 'email, first_name, last_name',
+                  display: 'popup'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
