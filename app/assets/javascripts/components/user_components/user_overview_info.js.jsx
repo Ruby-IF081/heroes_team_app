ae7afcd99e3f84b0b99f6530c29d40e02b35cc31
Misha7776 <vmisha7776@gmail.com>
2018-02-23 21:54:07 +0200

    Redused amount of lines in return statement

--- app/assets/javascripts/components/user_components/user_overview_info.js.jsx
+++ app/assets/javascripts/components/user_components/user_overview_info.js.jsx
@@ -1,33 +1,23 @@
 function Info(props) {
         return (
             <div className={"row col-sm-10"}>
-                <div className={"card-text col-sm-2"}>
-                    <h6>Full name:</h6>
-                </div>
+                <div className={"card-text col-sm-2"}><h6>Full name:</h6></div>
                 <div className={"card-text col-sm-10"}>
                     <p>{props.user.full_name}</p>
                 </div>
-                <div className={"card-text col-sm-2"}>
-                    <h6>User-email:</h6>
-                </div>
+                <div className={"card-text col-sm-2"}><h6>User-email:</h6></div>
                 <div className={"card-text col-sm-10"}>
                     <p>{props.user.email}</p>
                 </div>
-                <div className={"card-text col-sm-2"}>
-                    <h6>Role:</h6>
-                </div>
+                <div className={"card-text col-sm-2"}><h6>Role:</h6></div>
                 <div className={"card-text col-sm-10"}>
                     <p>{props.user.role}</p>
                 </div>
-                <div className={"card-text col-sm-2"}>
-                    <h6>Created at:</h6>
-                </div>
+                <div className={"card-text col-sm-2"}><h6>Created at:</h6></div>
                 <div className={"card-text col-sm-10"}>
                     <p>{props.user.created_at}</p>
                 </div>
-                <div className={"card-text col-sm-2"}>
-                    <h6>Tenant:</h6>
-                </div>
+                <div className={"card-text col-sm-2"}><h6>Tenant:</h6></div>
                 <div className={"card-text col-sm-10"}>
                     <p>{props.user.tenant_name}</p>
                 </div>
