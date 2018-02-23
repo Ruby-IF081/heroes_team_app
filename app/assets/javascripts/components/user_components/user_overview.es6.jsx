eabec9035f5ca0726e522b791e0f5022fb6fcf7d
Misha7776 <vmisha7776@gmail.com>
2018-02-23 21:47:38 +0200

    Refactored code in the overview file

---
+++ app/assets/javascripts/components/user_components/user_overview.es6.jsx
@@ -0,0 +1,24 @@
+class UserOverview extends React.Component {
+    render(){
+        const { user } = this.props;
+        return(
+            <div id={"overview"} className={"card"}>
+                <div className={"card-header"}>
+                    <h4 className={"card-title"}>Profile</h4>
+                </div>
+                <div className={"card-content"}>
+                    <div className={"card-body"}>
+                        <div className={"row"}>
+                            <div className={"card-image col-sm-2"}>
+                              <img src={user.avatar_url} alt={user.full_name} />
+                            </div>
+                            <div className={"card-text col-sm-10"}>
+                                <Info user={user} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
